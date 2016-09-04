c158e3ca265601b6c1a7d57b342d6f1e8aba08f4
playteka <zhoukeke@gmail.com>
2016-09-04 10:48:04 +0200

    analog_device_v2
    change the analog_device pin from numbers to options

--- public/blockly/app/playduino/blocks/analog.js
+++ public/blockly/app/playduino/blocks/analog.js
@@ -1,4 +1,5 @@
 // the analog sensor block with 0 as lower limit and 1023 as upper limit
+//depreciated by analog_device_v2 in 2016-09-04
 Blockly.Blocks['analog_device'] = {
 init: function() {
     this.setHelpUrl('http://www.example.com/');
@@ -15,6 +16,7 @@ init: function() {
 }
 };
 
+//depreciated by analog_device_v2 in 2016-09-04
 Blockly.JavaScript['analog_device'] = function(block) {
     var value_pin = Blockly.JavaScript.valueToCode(block, 'pin', Blockly.JavaScript.ORDER_ATOMIC);
     var text_var = block.getFieldValue('var');
@@ -34,7 +36,39 @@ Blockly.JavaScript['analog_device'] = function(block) {
 };
 
 
+// the analog sensor block with 0 as lower limit and 1023 as upper limit
+Blockly.Blocks['analog_device_v2'] = {
+init: function() {
+    this.setHelpUrl('http://www.example.com/');
+    this.setColour(120);
+    this.appendDummyInput()
+    .appendField(LANG["Analog:name"])
+    .appendField(new Blockly.FieldTextInput("analog"), "var")
+    .appendField(LANG["pin"])
+    .appendField(new Blockly.FieldDropdown([["A0", "A0"], ["A1", "A1"], ["A2", "A2"], ["A3", "A3"], ["A4", "A4"], ["A5", "A5"]]), "ANALOG_PIN");
+    this.setInputsInline(true);
+    this.setOutput(true, "device_type");
+    this.setTooltip('');
+}
+};
 
+Blockly.JavaScript['analog_device_v2'] = function(block) {
+    var dropdown_analog_pin = block.getFieldValue('ANALOG_PIN');
+    var text_var = block.getFieldValue('var');
+    
+    //push the analog variable into the led_var_set for the dropdown menu
+    all_devices.analog.push(text_var);
+    var inside_name = all_devices.create_inside_name(text_var);
+    all_devices.analog_dropdown.push([text_var,inside_name]);
+    
+    //push the servo variable into the object list for REPL
+    all_devices.objects.push(inside_name + " : " + inside_name);
+    
+    //Assemble code like "var sensor = new five.Sensor('A0');"
+    var code = "var " + inside_name + " = new five.Sensor('" + dropdown_analog_pin +"');";
+    
+    return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
 
 
 // event triggered when analog value changed
